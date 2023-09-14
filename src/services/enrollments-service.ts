54453cd228f4fb62bcd92bce4cd551d78e7790d0
Gabriel Goes Santos Rios <gabrielgoesrios@gmail.com>
2023-09-14 16:24:33 -0300

    feat: uso do requisito anterior para validar o cep na função createOrUpdateEnrollmentWithAddress

--- src/services/enrollments-service.ts
+++ src/services/enrollments-service.ts
@@ -17,7 +17,7 @@ async function getAddressFromCEP(cep : string) {
 async function getOneWithAddressByUserId(userId: number): Promise<GetOneWithAddressByUserIdResult> {
   const enrollmentWithAddress = await enrollmentRepository.findWithAddressByUserId(userId);
 
-  if (!enrollmentWithAddress) throw notFoundError();
+  if (!enrollmentWithAddress) throw invalidDataError('this registration does not have an address');
 
   const [firstAddress] = enrollmentWithAddress.Address;
   const address = getFirstAddress(firstAddress);
@@ -43,7 +43,7 @@ async function createOrUpdateEnrollmentWithAddress(params: CreateOrUpdateEnrollm
   enrollment.birthday = new Date(enrollment.birthday);
   const address = getAddressForUpsert(params.address);
 
-  // TODO - Verificar se o CEP é válido antes de associar ao enrollment.
+  await getAddressFromCEP(address.cep.replace('-', ''));
 
   const newEnrollment = await enrollmentRepository.upsert(params.userId, enrollment, exclude(enrollment, 'userId'));
 
