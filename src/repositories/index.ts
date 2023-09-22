3e35c17e2abc9b980fda34ad060aadf77d4ca875
Gabriel Goes Santos Rios <gabrielgoesrios@gmail.com>
2023-09-22 10:37:48 -0300

    fix: correção para seguir padrão do projeto

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -3,4 +3,4 @@ export * from './authentication-repository';
 export * from './enrollments-repository';
 export * from './events-repository';
 export * from './users-repository';
-export * from './tickets.repository';
+export * from './tickets-repository';
--- src/repositories/tickets-repository.ts
+++ src/repositories/tickets-repository.ts
@@ -1,4 +1,4 @@
-import { prisma } from './../config/database';
+import { prisma } from '../config/database';
 import { CreateTicket } from "@/services";
 
 type FindTicketType = CreateTicket
